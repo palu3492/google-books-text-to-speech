28047abdfc307cd4e15f439578a56ccdf99c563d
Alex Palumbo <palu3492@stthomas.edu>
2020-01-20 23:18:10 -0600

    Update content.js

--- js/content.js
+++ js/content.js
@@ -13,17 +13,49 @@ function setup(){
     });
 }
 
-let bookWords = [];
+let wordElements = [];
+let bookWords;
 function textToSpeech(){
-    let words = $.find('gbt');
-    words.forEach(word => {
-        if($(word).text().trim()){
-            bookWords.push(word);
+    let elements = $.find('gbt');
+    elements.forEach(wordElement => {
+        if($(wordElement).text().trim()){
+            wordElements.push(wordElement);
         }
     });
-    // // All book text for visible pages
+    // All book text for visible pages
     let bookText = $('.gb-text-reader').first().text();
+    bookText = bookText.replace(/\s+/g, ' ').trim();
+    bookWords = bookText.split(' ');
+    console.log(bookWords);
+    console.log(wordElements);
+    updateWordElements();
     speak(bookText);
+}
+
+let newWordElements = [];
+function updateWordElements(){
+    let w = 0;
+    for(let i=0; i<bookWords.length; i++){
+        console.log(bookWords[i]);
+        let c = 0;
+        newWordElements[w] = [];
+        while(elementText(w) !== bookWords[i]){
+            newWordElements[w].push(wordElements[w+c]);
+            c++;
+        }
+        w += c;
+        w++;
+        console.log(elementText(w));
+        console.log(w);
+    }
+}
+
+function elementText(w){
+    let text = "";
+    newWordElements[w].forEach(element => {
+        text += $(element).text().trim();
+    });
+    return text;
 }
 
 let index = 0;
@@ -45,10 +77,15 @@ function stop(){
 
 function highlightText(){
     if(index > 0){
-        $(bookWords[index-1]).removeClass('highlight');
+        // $(newWordElements[index-1]).removeClass('highlight');
         // $(bookWords[index-1]).addClass('oldHighlight');
+        newWordElements[index-1].forEach(element => {
+            $(element).removeClass('highlight');
+        });
     }
-    $(bookWords[index]).addClass('highlight');
+    newWordElements[index].forEach(element => {
+        $(element).addClass('highlight');
+    });
 }
 
 // let go = true;
